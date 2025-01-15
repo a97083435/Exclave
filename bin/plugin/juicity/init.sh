ce9a6a7bf65753924bda720ac6b686fefde45f89
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-01-15 23:14:59 +0800

    refine build procedure

--- bin/plugin/juicity/init.sh
+++ bin/plugin/juicity/init.sh
@@ -2,12 +2,7 @@
 
 source "bin/init/env.sh"
 
-export CGO_ENABLED=1
-export GOOS=android
-
 CURR="plugin/juicity"
 CURR_PATH="$PROJECT/$CURR"
 
-git submodule update --init "$CURR/*"
 cd $CURR_PATH/src/main/go/juicity
-go mod download -x
--- bin/plugin/mieru/init.sh
+++ bin/plugin/mieru/init.sh
@@ -2,12 +2,7 @@
 
 source "bin/init/env.sh"
 
-export CGO_ENABLED=1
-export GOOS=android
-
 CURR="plugin/mieru"
 CURR_PATH="$PROJECT/$CURR"
 
-git submodule update --init "$CURR/*"
 cd $CURR_PATH/src/main/go/mieru
-go mod download -x
